27b47b5d7e08092db84195ad9393e725ecf36358
Felipe Celis <fecelis@hotmail.com>
2015-10-15 20:34:46 -0500

    Create dojo2-1.scala

---
+++ dojo2-1.scala
@@ -0,0 +1,11 @@
+object funcional{
+     def main(args: Array[String]) 
+     {
+     	var lista = List (1,2,3,4)
+     	lista.map((x:Int)=>println(x+4))	
+     	println(suma2(2,2))
+     }
+     def suma(x:Int,y:Int)=x+y
+     //FUNCIONAL
+     val suma2=(x:Int,y:Int)=>x+y
+}
